a9aa250283c7de34e133137a2741c17b47880c90
Stefan Karlsson <stefankarlsson1230@gmail.com>
2026-05-30 08:09:24 +0200

    Finished exercise 3 and 4

--- mySolution.sql
+++ mySolution.sql
@@ -53,6 +53,7 @@ CREATE TABLE orderrows (
 		ON DELETE CASCADE
 );
 
+
 -- Exercise 2
 INSERT INTO customers (first_name, last_name, billing_address, billing_zip, billing_city, delivery_address, delivery_zip, delivery_city, email, phone)
 VALUES 
@@ -72,10 +73,75 @@ VALUES
 -- Exercise 3
 -- On 2025-01-01, Jonas Gray purchases Interstellar and Pulp Fiction
 INSERT INTO orders (order_date, customer_id)
-VALUES ()
+VALUES ('2025-01-01', (SELECT id FROM customers WHERE first_name = 'Jonas' AND last_name = 'Gray'));
 
+INSERT INTO orderrows (order_id, movie_id, price)
+VALUES 
+(
+	(SELECT max(id) FROM orders),
+	(SELECT id FROM movies WHERE title = 'Interstellar'),
+	(SELECT price FROM movies WHERE title = 'Interstellar')
+),
+(
+	(SELECT max(id) FROM orders),
+	(SELECT id FROM movies WHERE title = 'Pulp Fiction'),
+	(SELECT price FROM movies WHERE title = 'Pulp Fiction')
+);
 
 -- On 2025-12-15, Peter Birro purchases 2 copies of The Wolf of Wall Street.
+INSERT INTO orders (order_date, customer_id)
+VALUES ('2025-12-15', (SELECT id FROM customers WHERE first_name = 'Peter' AND last_name = 'Birro'));
+
+INSERT INTO orderrows (order_id, movie_id, price)
+VALUES 
+(
+	(SELECT max(id) FROM orders),
+	(SELECT id FROM movies WHERE title = 'The Wolf of Wall Street'),
+	(SELECT price FROM movies WHERE title = 'The Wolf of Wall Street')
+),
+(
+	(SELECT max(id) FROM orders),
+	(SELECT id FROM movies WHERE title = 'The Wolf of Wall Street'),
+	(SELECT price FROM movies WHERE title = 'The Wolf of Wall Street')
+);
+
 -- On 2026-03-20, Jonas Gray purchased The Wolf of Wall Street 
+INSERT INTO orders (order_date, customer_id)
+VALUES ('2026-03-20', (SELECT id FROM customers WHERE first_name = 'Jonas' AND last_name = 'Gray'));
+
+INSERT INTO orderrows (order_id, movie_id, price)
+VALUES 
+(
+	(SELECT max(id) FROM orders),
+	(SELECT id FROM movies WHERE title = 'The Wolf of Wall Street'),
+	(SELECT price FROM movies WHERE title = 'The Wolf of Wall Street')
+);
+
+
+-- Exercise 4
+-- Write a query that changes the price of all movies made in 2014 to 169 kr.
+UPDATE movies 
+SET price = 169
+WHERE release_year = 2014;
+
+
+-- Exercise 5
+-- Write queries for the following SELECT operations
+-- Get first_name, Last_name, phone and email to all customers.
+
+
+
+-- Get all movies, ordered by year from newest to oldest.
+-- Get all movie titles, ordered by price, from cheapest to most expensive.
+-- Get first_name, Last_name, delivery__address, delivery_zip, delivery_city for all customers who bought The Wolf of Wall Street.
+--Get Id, Date, customer (first_name, last_name) and total cost of every individual order.
+-- (Optional) Get customer (first_name, last_name), total number of movies ordered by this customer, number of orders by this customer and total cost of all orders by this customer.
+-- (Optional) Get number of orders and total cost for all orders in the database.
+
+
+
+
+
+
 
 
